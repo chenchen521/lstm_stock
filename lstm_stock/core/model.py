6ce42dc645b858fd955d65af88768d3ed6ecf9ae
Todd <xxbsh2010@163.com>
2018-09-27 12:01:32 +0800

    fuck

---
+++ lstm_stock/core/model.py
@@ -0,0 +1,79 @@
+import os
+import math
+import numpy as np
+import datetime as dt
+from numpy import newaxis
+from core.utils import Timer
+from keras.layers import Dense, Activation, Dropout, LSTM
+from keras.models import Sequential, load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+class Model():
+	"""A class for an building and inferencing an lstm model"""
+
+	def __init__(self):
+		self.model = Sequential()
+
+	def load_model(self, filepath):
+		print('[Model] Loading model from file %s' % filepath)
+		self.model = load_model(filepath)
+
+	def build_model(self, configs):
+		timer = Timer()
+		timer.start()
+
+		for layer in configs['model']['layers']:
+			neurons = layer['neurons'] if 'neurons' in layer else None
+			dropout_rate = layer['rate'] if 'rate' in layer else None
+			activation = layer['activation'] if 'activation' in layer else None
+			return_seq = layer['return_seq'] if 'return_seq' in layer else None
+			input_timesteps = layer['input_timesteps'] if 'input_timesteps' in layer else None
+			input_dim = layer['input_dim'] if 'input_dim' in layer else None
+
+			if layer['type'] == 'dense':
+				self.model.add(Dense(neurons, activation=activation))
+			if layer['type'] == 'lstm':
+				self.model.add(LSTM(neurons, input_shape=(input_timesteps, input_dim), return_sequences=return_seq))  #100，(49,2), 
+			if layer['type'] == 'dropout':
+				self.model.add(Dropout(dropout_rate))
+
+		self.model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'])
+
+		print('[Model] Model Compiled')
+		timer.stop()
+
+
+	def train_generator(self, data_gen, epochs, batch_size, steps_per_epoch, save_dir):
+		timer = Timer()
+		timer.start()
+		print('[Model] Training Started')
+		print('[Model] %s epochs, %s batch size, %s batches per epoch' % (epochs, batch_size, steps_per_epoch))
+		
+		save_fname = os.path.join(save_dir, '%s-e%s.h5' % (dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(epochs)))
+		callbacks = [
+			ModelCheckpoint(filepath=save_fname, monitor='loss', save_best_only=True)
+		]
+		self.model.fit_generator(
+			data_gen,
+			steps_per_epoch=steps_per_epoch,
+			epochs=epochs,
+			callbacks=callbacks,
+			workers=1
+		)
+		
+		print('[Model] Training Completed. Model saved as %s' % save_fname)
+		timer.stop()
+
+	def predict_sequences_multiple(self, data, window_size, prediction_len):
+		#Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+		prediction_seqs = []
+        #data.shape = (655,49,2), range(13), 655個預測單元，分爲13組
+		for i in range(int(len(data)/prediction_len)):  #第i組
+			curr_frame = data[i*prediction_len]  #data[0], data[50], data[100], (49,2)
+			predicted = []
+			for j in range(prediction_len):  #range(50)
+				predicted.append(self.model.predict(curr_frame[newaxis,:,:])[0,0])
+				curr_frame = curr_frame[1:]
+				curr_frame = np.insert(curr_frame, [window_size-2], predicted[-1], axis=0)
+			prediction_seqs.append(predicted)
+		return prediction_seqs
